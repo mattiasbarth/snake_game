57fe93a6b38c4221a8393807899181fbba2875c9
Mattias Barth <mattias.barth@gmail.com>
2026-04-01 14:56:58 +0200

    add highscore function in scoreboard, add reset function in snake, update main

--- scoreboard.py
+++ scoreboard.py
@@ -9,6 +9,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highscore = 0
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -17,12 +18,20 @@ class Scoreboard(Turtle):
 
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.highscore}", align=ALIGNMENT, font=FONT)
 
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("Game Over", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("Game Over", align=ALIGNMENT, font=FONT)
 
 
     def increase_score(self):
